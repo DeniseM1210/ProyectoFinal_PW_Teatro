b8774b95b8abcf1ab7518dbb12150b5da4d36a50
Denise <denisemorales8764@gmail.com>
2023-09-23 23:57:15 -0600

    Se agregó funcionalidad a algunos botones del header

--- scripts/php/vistas/header.php
+++ scripts/php/vistas/header.php
@@ -3,9 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/dae357c832.js" crossorigin="anonymous"></script>
+    <title>Inicio</title>
 </head>
 <body>
-    
+<nav id="header" class="navbar navbar-expand-lg sticky-top" style="background: #FFA7A7 !important;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Miembros</a>
+            <a class="navbar-brand" href="#">Lista de obras</a>
+            <a class="navbar-brand" href="#">Venta de boletos</a>
+            <a class="navbar-brand" href="#">Patronos</a>
+            <a class="navbar-brand" href="#">Patrocinios</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="btn" style="background-color: #4D0007; color: white;" data-bs-toggle="" href="../../../index.html"><strong>
+                            Salir
+                        </strong></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <footer class="text-center text-white fixed-bottom" style="background-color: #FFA7A7;">
+        <div class="container p-4"></div>
+      </footer>
 </body>
 </html>
